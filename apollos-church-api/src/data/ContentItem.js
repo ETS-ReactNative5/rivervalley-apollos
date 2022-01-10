6fe9066f46c177c778242aabf48f49f24de534e6
nlewis84 <nathan@differential.com>
2022-01-10 09:57:10 -0600

    feat: reorder of Spotify Playlist

--- apollos-church-api/src/data/ContentItem.js
+++ apollos-church-api/src/data/ContentItem.js
@@ -1,6 +1,24 @@
 import { ContentItem } from '@apollosproject/data-connector-postgres';
 
-const { dataSource, schema, models, migrations } = ContentItem;
+const { schema, models, migrations } = ContentItem;
+
+class dataSource extends ContentItem.dataSource {
+  baseItemByChannelCursor = this.byContentChannelId;
+
+  getFeatures = async (item) => {
+    const features = await super.getFeatures(item);
+
+    // This moves the Webview feature above the journal entries
+    if (features[features.length - 1].dataValues.type === 'Webview') {
+      const tempFeature = features[features.length - 1];
+
+      features[features.length - 1] = features[features.length - 3];
+      features[features.length - 3] = tempFeature;
+    }
+
+    return features;
+  };
+}
 
 const resolver = {
   ...ContentItem.resolver,
